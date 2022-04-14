52e8f9103933c07b8c6ec86aee1ff5df0f55e344
erenmestan <ermestan@gmail.com>
2022-04-15 00:05:35 +0300

    Added Mapping and New DTOs

--- NLayer.Core/DTOs/ProductFeatureDto.cs
+++ NLayer.Core/DTOs/ProductFeatureDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NLayer.Core.DTOs
 {
-    public class PruductFeatureDto
+    public class ProductFeatureDto
     {
         public int Id { get; set; }
         public string? Color { get; set; }
