6a79980cbe2c829fc1b886e25920c082d2e76761
erenmestan <ermestan@gmail.com>
2022-04-19 00:10:24 +0300

    Created CategoryApiService and ProductApiService

--- NLayer.Api/Modules/RepoServiceModule.cs
+++ NLayer.Api/Modules/RepoServiceModule.cs
@@ -31,7 +31,7 @@ namespace NLayer.Api.Modules
             builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Repository")).AsImplementedInterfaces().InstancePerLifetimeScope();
 
             builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly).Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces().InstancePerLifetimeScope();
-            builder.RegisterType<ProductServiceWithCaching>().As<IProductService>();
+            //builder.RegisterType<ProductServiceWithCaching>().As<IProductService>();
         }
     }
 }
--- NLayer.Caching/ProductServiceWithCaching.cs
+++ NLayer.Caching/ProductServiceWithCaching.cs
@@ -83,12 +83,12 @@ namespace NLayer.Caching
             return Task.FromResult(product);
         }
 
-        public Task<List<ProductWithCategoryDto>> GetProductsWithCategory()
+        public Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWithCategory()
         {
             var products = _memoryCache.Get<IEnumerable<Product>>(CacheProductKey);
             var productsWithCategoryDto = _mapper.Map<List<ProductWithCategoryDto>>(products);
 
-            return Task.FromResult(productsWithCategoryDto);
+            return Task.FromResult(CustomResponseDto<List<ProductWithCategoryDto>>.Success(200, productsWithCategoryDto));
         }
 
         public async void RemoveRange(IEnumerable<Product> entities)
