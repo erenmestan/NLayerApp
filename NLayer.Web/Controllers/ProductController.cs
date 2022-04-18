338bdab3c521821f32d747141ad7a4d0790cfa76
erenmestan <ermestan@gmail.com>
2022-04-18 12:45:20 +0300

    Created Mvc Controller and view

---
+++ NLayer.Web/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.Services;
+
+namespace NLayer.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _services;
+
+        public ProductController(IProductService service)
+        {
+            _services=service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
+            return View(await _services.GetProductsWithCategory());
+        }
+    }
+}
