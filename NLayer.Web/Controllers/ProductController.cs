27038273831221c058355312b62b614c240a4f72
erenmestan <ermestan@gmail.com>
2022-04-18 16:42:24 +0300

    Added Save View

--- NLayer.Web/Controllers/ProductController.cs
+++ NLayer.Web/Controllers/ProductController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NLayer.Core;
+using NLayer.Core.DTOs;
 using NLayer.Core.Services;
 
 namespace NLayer.Web.Controllers
@@ -6,16 +10,40 @@ namespace NLayer.Web.Controllers
     public class ProductController : Controller
     {
         private readonly IProductService _services;
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public ProductController(IProductService service)
+        public ProductController(IProductService services, ICategoryService categoryService, IMapper mapper)
         {
-            _services=service;
+            _services=services;
+            _categoryService=categoryService;
+            _mapper=mapper;
         }
 
         public async Task<IActionResult> Index()
-        {
-            
+        {   
             return View(await _services.GetProductsWithCategory());
+        }
+        public async Task<IActionResult> Save()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            var categoryiesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+            ViewBag.categories = new SelectList(categoryiesDto, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _services.AddAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+            }
+            var categories = await _categoryService.GetAllAsync();
+            var categoryiesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+            ViewBag.categories = new SelectList(categoryiesDto, "Id", "Name");
+            return View();
         }
     }
 }
